3e0d18b1f4e193d00cd498eed88905c79383cc4f
RhodaCodes <93567059+Rhoda-Codes@users.noreply.github.com>
2022-09-29 00:52:06 +0300

    function that prints a string, followed by a new line.

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,37 +1,19 @@
 #include "main.h"
 
-
-
 /**
+ * _puts_recursion - prints a string
  *
- *  * _puts_recursion - prints a string
- *
- *   *
- *
- *    * @s: string
- *
- *     */
-
+ * @s: string
+ */
 void _puts_recursion(char *s)
-
 {
-
-		if (*s == '\0')
-
-				{
-
-							_putchar('\n');
-
-								}
-
-			else
-
-					{
-
-								_putchar(*s);
-
-										_puts_recursion(s + 1);
-
-											}
-
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
